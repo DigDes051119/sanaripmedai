142f1ac95091db301533fb9a77d366271853e648
Your Name <you@example.com>
2026-07-12 14:14:33 +0600

    fix: redis foreground mode instead of daemonize in Docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Запуск локального сервера Redis в фоновом режиме (направляем пути в /tmp во избежание ошибок прав доступа на Hugging Face)
 echo "Запуск Redis-сервера..."
-redis-server --daemonize yes --dir /tmp --dbfilename redis-dump.rdb --pidfile /tmp/redis-server.pid --logfile /tmp/redis.log
+redis-server --dir /tmp --dbfilename redis-dump.rdb --pidfile /tmp/redis-server.pid --logfile /tmp/redis.log &
 
 
 # Проверка, что Redis запустился
